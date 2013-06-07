f97eab14264840265eab152e8ebddb22375e45fd
Hans Marasigan <hansmarasigan@gmail.com>
2013-06-07 11:11:34 -0700

    cleaned code for unneeded comments

--- src/edu/ucsb/cs56/projects/games/roguelike/LogicEngine.java
+++ src/edu/ucsb/cs56/projects/games/roguelike/LogicEngine.java
@@ -163,8 +163,7 @@ public class LogicEngine {
 		//player isn't movable, start attack for player
 	    if(floor[x][y] instanceof Monster){	
 		if(floor[xOrig][yOrig] instanceof Player){
-			    //thePlayer.attacking(listOfMonsters[x][y]);
-				return false;	
+							return false;	
 			}else{	
 				return false;	
 			}
@@ -172,20 +171,10 @@ public class LogicEngine {
 				
 		//monster isn't movable, start attack for monster
 	    if(floor[x][y] instanceof Player && floor[xOrig][yOrig] instanceof Monster){
-		    //listOfMonsters[xOrig][yOrig].attacking(thePlayer);
-			return false;
+		    			return false;
 		}
 		
 
-		// monster or player is movable	
-
-	//	floor[x][y] = floor[xOrig][yOrig];
-	//	floor[xOrig][yOrig] = null;
-
-	//	int[] position = {x,y};
-       // if(floor[x][y] instanceof Player){
-        //	thePlayer.setPlayerPosition(position);
-        //}
 		return true;
 	}
 	
@@ -290,8 +279,8 @@ public class LogicEngine {
 		 if(floor[xPos][yPos]==null){
 		     
 		     floor[xPos][yPos] = new Monster(numGenerator.nextInt(2)+1);
-		     // listOfMonsters[xPos][yPos] = (Monster) floor[xPos][yPos];
-		     // listOfMonsters[xPos][yPos].setMonsterPosition(xPos,yPos);
+		     listOfMonsters[xPos][yPos] = (Monster) floor[xPos][yPos];
+		     listOfMonsters[xPos][yPos].setMonsterPosition(xPos,yPos);
 		     }
 		     
 		 
