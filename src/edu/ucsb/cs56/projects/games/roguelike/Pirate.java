7d5977d9322ec0a00d71371653639e092032cd45
Rick Lee <ricklee@umail.ucsb.edu>
2014-02-21 21:55:11 -0800

    Added 3 new monsters: Pirate, Snake, and Zombie

---
+++ src/edu/ucsb/cs56/projects/games/roguelike/Pirate.java
@@ -0,0 +1,70 @@
+package edu.ucsb.cs56.projects.games.roguelike;
+
+import java.util.Random;
+
+/**
+ *@author Clayven Anderson
+ *@author Hans Marasigan & Richard Nguyen
+ *@author Rick Lee
+ *@version cs56 Winter 14
+ */
+/**
+ *This class represents a monster called a Pirate that is a medium health, medium damaged melee creature. Pirate have icon P
+ */
+
+public class Pirate extends Monster{ 
+    private char icon;
+
+    /**
+     *creates a Pirate with 20 hitPoints and 15 attack with no random movement
+     *with icon P
+     */
+    public Pirate(){
+	super(20,15,0,20);    
+	this.setIcon('P');
+    }
+    /**
+     * creates a pirate with 20 HP and 15 attack with icon P
+     * @param randomMovement whether or not the monster will move randomly
+     */
+    public Pirate(int typeOfMovement){
+	super(20,15,typeOfMovement,20);
+	this.setIcon('P');
+    }
+    
+    /**
+     * creates a pirate with hit points, attack, and random movement with icon P
+     * @param hitPoints the Pirate's hitPoints
+     * @param attack the Pirate's attack
+     * @param randomMovement whether or not the Pirate will move randomly or not
+     */
+    public Pirate(int hitPoints, int attack, int typeOfMovement){
+	super(hitPoints, attack, typeOfMovement,20);
+	this.setIcon('P');
+    }
+    /**
+     *creates a Pirate to the parameters given to it
+     * @param hp the Pirate's hitPoints
+     * @param att the Pirate's attack
+     * @param typeOfMove whether or not the Pirate will move randomly or not
+     * @param points the amount of points the Pirate is worth
+     */
+    public Pirate(int hitPoints,int att,int typeOfMove, int points){
+	super(hitPoints,att,typeOfMove,points); 
+	this.setIcon('P');
+    }
+    /** 
+     *This is the getter to figure out what piece icon it is. 
+     */ 
+    @Override public char getIcon(){
+	return this.icon;
+    }
+    /** 
+     *This is the setter for the Icon it will be 
+     *@param NewIcon is the icon of piece that will be in the game 
+     */ 
+    @Override public void setIcon(char NewIcon){
+	    this.icon=NewIcon;
+
+    }
+}
---
+++ src/edu/ucsb/cs56/projects/games/roguelike/Zombie.java
@@ -0,0 +1,70 @@
+package edu.ucsb.cs56.projects.games.roguelike;
+
+import java.util.Random;
+
+/**
+ *@author Clayven Anderson
+ *@author Hans Marasigan & Richard Nguyen
+ *@author Rick Lee
+ *@version cs56 Winter 14
+ */
+/**
+ *This class represents a monster called a zombie that is a medium health, low damaged melee creature. Zombie have icon Z
+ */
+
+public class Zombie extends Monster{ 
+    private char icon;
+
+    /**
+     *creates a zombie with 20 hitPoints and 5 attack with no random movement
+     *with icon Z
+     */
+    public Zombie(){
+	super(20,5,0,15);    
+	this.setIcon('Z');
+    }
+    /**
+     * creates a Zombie with 20 HP and 5 attack with icon Z
+     * @param randomMovement whether or not the monster will move randomly
+     */
+    public Zombie(int typeOfMovement){
+	super(20,5,typeOfMovement,15);
+	this.setIcon('Z');
+    }
+    
+    /**
+     * creates a zombie with hit points, attack, and random movement with icon Z
+     * @param hitPoints the Zombie's hitPoints
+     * @param attack the Zombie's attack
+     * @param randomMovement whether or not the Zombie will move randomly or not
+     */
+    public Zombie(int hitPoints, int attack, int typeOfMovement){
+	super(hitPoints, attack, typeOfMovement,15);
+	this.setIcon('Z');
+    }
+    /**
+     *creates a Zombie to the parameters given to it
+     * @param hp the Zombie's hitPoints
+     * @param att the Zombie's attack
+     * @param typeOfMove whether or not the Zombie will move randomly or not
+     * @param points the amount of points the Zombie is worth
+     */
+    public Zombie(int hitPoints,int att,int typeOfMove, int points){
+	super(hitPoints,att,typeOfMove,points); 
+	this.setIcon('Z');
+    }
+    /** 
+     *This is the getter to figure out what piece icon it is. 
+     */ 
+    @Override public char getIcon(){
+	return this.icon;
+    }
+    /** 
+     *This is the setter for the Icon it will be 
+     *@param NewIcon is the icon of piece that will be in the game 
+     */ 
+    @Override public void setIcon(char NewIcon){
+	    this.icon=NewIcon;
+
+    }
+}
