ef184ad381f5fd4cb3a333313326092ce5cc1db5
Erick Suarez <mr.ericksuarez@gmail.com>
2017-11-24 20:59:38 -0800

    Change x and y range

--- src/edu/ucsb/cs56/projects/games/roguelike/LogicEngine.java
+++ src/edu/ucsb/cs56/projects/games/roguelike/LogicEngine.java
@@ -421,10 +421,12 @@ public class LogicEngine {
 
         // create room with randomized values
         for(int i = 0; i < 8; i++) {
+            int range = (max - min) + 1;
+            return (int)(Math.random() * range) + min;
             w = (int)(Math.random() * (floorWidth / 3))+5;
             h = (int)(Math.random() * (floorHeight / 3))+5;
-            x = (int)(Math.random() * (floorWidth - w - 1) + 1);
-            y = (int)(Math.random() * (floorHeight - h - 1) + 1);
+            x = (int)(Math.random() * (floorWidth - w - 1));
+            y = (int)(Math.random() * (floorHeight - h - 1));
 
             Room newRoom = new Room(x, y, w, h);
 
